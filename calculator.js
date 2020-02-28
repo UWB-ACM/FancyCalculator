067609b0aab70bafc155c8abfc8dc47fcfefa5c7
Sam Wolf <sam1008@icloud.com>
2020-02-28 14:32:01 -0800

    Update calculator.js
    Co-Authored-By: Erica F <58118933+Erica-Fer@users.noreply.github.com>

--- calculator.js
+++ calculator.js
@@ -237,7 +237,6 @@ $( document ).ready(function() {
     // TODO: Fix theme 7 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
-    //   - Uncomment the call to addOption (remove the `//`)
     addOption("Wolf theme", "linear-gradient(blue, orange)", "blue", "white");
 
     // TODO: Fix theme 8 - To fix this:
