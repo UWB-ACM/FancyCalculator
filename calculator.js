e026c48086be1fe22411a1c4be732aa5cee92a5b
Chapman Tse <tch222000@hotmail.com>
2020-02-28 14:13:58 -0800

    Cheuk-Hang Tse Fix #23 bug

--- calculator.js
+++ calculator.js
@@ -334,7 +334,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("CheukHang theme", "linear-gradient(red, purple)", "blue", "white");
 
     // TODO: Fix theme 24 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
