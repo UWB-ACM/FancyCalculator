6752087948fab7c119ca905742b25bc2ef819789
Jessica Nguyen <jessica.nguyen0107@gmail.com>
2020-02-28 14:38:58 -0800

    Fixed #47

--- calculator.js
+++ calculator.js
@@ -250,7 +250,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    addOption("Jessica theme", "linear-gradient(white, purple)", "pink", "grey");
+    // addOption("Jessica theme", "linear-gradient(white, purple)", "pink", "grey");
 
     // TODO: Fix theme name0 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
@@ -478,7 +478,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("Jessica theme", "linear-gradient(white, purple)", "pink", "gray");
 
     // TODO: Fix theme 48 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
