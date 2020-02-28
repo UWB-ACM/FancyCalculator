6e795fdf183c94230f71a25bcbe4e27d69238763
hollowsunsets <isabelgiang@gmail.com>
2020-02-28 13:55:35 -0800

    Updated theme 42

--- calculator.js
+++ calculator.js
@@ -448,7 +448,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("Hoiyoiyoiyoyioyioyiyoiyoiyoyioyioyioyiyoiyoyioyioyiyoi", "linear-gradient(lime-green, purple)", "lime-green", "purple");
 
     // TODO: Fix theme 43 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
