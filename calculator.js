b89d335c405ea068eb9e5789e940fd263af3e779
erica-fer <ericafer@uw.edu>
2020-02-28 14:48:00 -0800

    fixed problem 26

--- calculator.js
+++ calculator.js
@@ -351,7 +351,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.	
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("based king", "linear-gradient(yellow, yellow)", "yellow", "yellow");
 
     // TODO: Fix theme 27 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
