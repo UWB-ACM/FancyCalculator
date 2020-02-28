0bb514de55c79b16659502fdf1f3aaed1d62e64d
Estefano Felipa <46825977+estefanofc@users.noreply.github.com>
2020-02-28 14:43:34 -0800

    My bug fix (#156)

--- calculator.js
+++ calculator.js
@@ -243,7 +243,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("estefano", "linear-gradient(black, white)", "red", "red");
 
     // TODO: Fix theme 9 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
