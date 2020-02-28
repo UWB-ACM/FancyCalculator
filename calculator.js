74061d31ba9ff25934591a0a94599d8dda8ec443
nerdromere <36607004+nerdromere@users.noreply.github.com>
2020-02-28 14:55:20 -0800

    My bug fix, changed colors, created annother option (#154)

--- calculator.js
+++ calculator.js
@@ -375,7 +375,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("nerdromere feature", "linear-gradient(green, blue)", "gold", "purple");
 
     // TODO: Fix theme 31 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
