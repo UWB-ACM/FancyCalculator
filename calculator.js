46627cb90789ca2f0b4cf2cd016369aaf1ba5719
Alex Putman <43959556+pointblank1123@users.noreply.github.com>
2020-03-16 17:19:53 -0600

    Edited theme 35, new theme (#167)
    Edited theme 35, new theme

--- calculator.js
+++ calculator.js
@@ -405,7 +405,7 @@ $( document ).ready(function() {
     //   - Change "theme name" to a catchy name (can be your name)
     //   - Change the colors blue and red with colors of your choice.
     //   - Uncomment the call to addOption (remove the `//`)
-    // addOption("theme name", "linear-gradient(blue, red)", "red", "red");
+    addOption("Daylight", "linear-gradient(yellow, blue)", "red", "red");
 
     // TODO: Fix theme 36 - To fix this:
     //   - Change "theme name" to a catchy name (can be your name)
